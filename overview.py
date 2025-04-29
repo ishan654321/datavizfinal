5584afa953b0f4bab791f77d215ba135a31ab452
Ishan Vasavada <80118005+ishan654321@users.noreply.github.com>
2025-04-29 13:40:49 -0400

    Update overview.py

--- overview.py
+++ overview.py
@@ -18,3 +18,9 @@ def overview():
         components.html(html, height=800, scrolling=True)
     st.header('Values in Cluster')
     st.image('Screenshot 2025-04-28 213043.png')
+    st.markdown('''- It was a huge amount of data and to deal with it was big challenge.
+- Colab, Kaggle, Jupyter notebook was not helpful when we tried to load 8.6 millions of rows with 1.1 GB size of csv file into data frame. 
+The visualizations were taking even longer time to generate.
+- The dataset lacks the event descriptions like hurricane or storm passing area and how affected the area was.
+- The dataset is also between limited years 2016 to 2022 which could be insufficient to predict climate change or any such changes.
+''')
